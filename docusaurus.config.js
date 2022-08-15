d9d7966fd130cfefd8a7abd1f70d918f1c3eff86
kumarnitesh2000 <46937660+kumarnitesh2000@users.noreply.github.com>
2022-08-15 15:56:34 +0000

    chnage in editurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/blogs-era/sde-babu-blog-content/tree/main/docs/',
+            'https://github.com/blogs-era/sde-babu-blog-content/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
